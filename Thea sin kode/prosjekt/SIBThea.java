1c04f22bd6a8a3d59a30c39c370c5c3671efa083
TheaIsla <thea.isla@gmail.com>
2017-03-05 20:55:09 +0100

    ny mappe

--- Thea sin kode/prosjekt/SIBThea.java
+++ Thea sin kode/prosjekt/SIBThea.java
@@ -9,8 +9,9 @@ import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.sparql.vocabulary.FOAF;
 import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.OWL;
 
-public class SIBThea {
+public class SIBThea extends Generelt {
 	
 	public static void main(String[] args) {
 		Model basemodel = ModelFactory.createDefaultModel();
@@ -20,67 +21,85 @@ public class SIBThea {
 		String dbpedia = "http://dbpedia.org/resource/";
 		String base = "http://example.org/";
 		String schema = "http://schema.org/";
+		String owl = "";
 		
 		// Properties
 		Property postalAddress = model.createProperty(schema + "PostalAddress");
-		Property phoneNr = model.createProperty(base + "phoneNumber");
-		Property email = model.createProperty(base + "email");
+		Property telephone = model.createProperty(schema + "telephone");
+		Property email = model.createProperty(schema + "email");
 		
 		// SIB
-		Resource SIB = model.createResource("www.sib.no");
+		Resource SIB = model.createResource(owl + "SIB");
 
+		// Studentsenteret
+		Resource studentsenteret = model.createResource(owl + "studentsenteret");
 		
-		//Vektertorget
-		Resource vektertorget = model.createResource(base + "vektertorget");
-		
-		vektertorget.addProperty(postalAddress, "Christiessgt. 13, 5015 Bergen, Norge");
-		vektertorget.addProperty(phoneNr, "55545180");
-		vektertorget.addProperty(email, "vektertorget.treningssenter@sib.no");
-
-
-		
-		
-		// Give name
-		cadeTracy.addProperty(FOAF.name, "Cade Tracy");
-		inesD.addProperty(FOAF.name, "Ines Dominguez");
-	
 		// Relationship
-		cadeTracy.addProperty(FOAF.knows, inesD);
-		inesD.addProperty(FOAF.knows, cadeTracy);
+		SIB.addProperty(FOAF.knows, studentsenteret); 
+		studentsenteret.addProperty(FOAF.knows, SIB);
 		
-		// Bachelor from
-		Property bachelorFrom = model.createProperty(schema + "CollegeOrUniversity");
-		cadeTracy.addProperty(bachelorFrom, "Berkley");
-		inesD.addProperty(bachelorFrom, "University of Valencia");
-	
-		// Graduation year
-		Property graduationYear = model.createProperty(base	+ "graduationYear");
-		cadeTracy.addProperty(graduationYear, "2011");
-		inesD.addProperty(graduationYear, "2015");
+		// Contact info
+		studentsenteret.addProperty(postalAddress, "Parkveien 1, 5007 Bergen, Norge");
+		studentsenteret.addProperty(telephone, "55545124");
+		studentsenteret.addProperty(email, "studentsenteret@sib.no");
 		
-		// Address
-		Property postalAddress = model.createProperty(schema + "PostalAddress");
-		cadeTracy.addProperty(postalAddress, "1516 Henry Street, Berkely, California 94709, USA");
-		inesD.addProperty(postalAddress, "Carrer de la Guardia Civil 20, 46020 Valencia, Spain");
+		//Åpningstider
+		Property openingHours = model.createProperty(schema + "openingHours");
+		Property mo = model.createProperty(schema + "Mandag");
+		Property tu = model.createProperty(schema + "Tirsdag");
+		Property we = model.createProperty(schema + "Onsdag");
+		Property th = model.createProperty(schema + "Torsdag");
+		Property fr = model.createProperty(schema + "Fredag");
+		Property sa = model.createProperty(schema + "Lørdag");
+		Property su = model.createProperty(schema + "Søndag");
 		
-		// Interests List		
-		Property propIntrests = model.createProperty(base + "intrests");
-		Resource resBikeriding = model.createResource(dbpedia + "BikeRiding");
-		Resource resMusic = model.createResource(dbpedia + "Music");
-		Resource resTravelling = model.createResource(dbpedia + "Travelling");
-		Resource resBirds = model.createResource(dbpedia + "Birds");
-		Resource resEcology = model.createResource(dbpedia + "Ecology");
-		Resource resEnviroment = model.createResource(dbpedia + "Enviroment");
-		Resource resPhotography = model.createResource(dbpedia + "Photography");
+		studentsenteret.addProperty(mo, "07:00 - 22:30");
+		studentsenteret.addProperty(tu, "07:00 - 22:30");
+		studentsenteret.addProperty(we, "07:00 - 22:30");
+		studentsenteret.addProperty(th, "07:00 - 22:30");
+		studentsenteret.addProperty(fr, "07:00 - 21:30");
+		studentsenteret.addProperty(sa, "09:00 - 18:00");
+		studentsenteret.addProperty(su, "10:00 - 20:00");
+		
+		Resource[]ssListOfOpeningHours = {mo, tu, we, th, fr, sa, su};
+		Resource ssOpeningHours = model.createList(ssListOfOpeningHours);
+		studentsenteret.addProperty(openingHours, ssOpeningHours);
+		
+		// Class type
+		Resource spinningtime = model.createResource(owl + "spinning");
+		Resource saltime = model.createResource(owl + "saltime");
+		Resource bassengtime = model.createResource(owl + "bassengtimer");
+		Resource rotime = model.createResource(owl + "rotime");
+		
+		
+		// Classes 	
+		//Property workoutClass = model.createProperty(owl + "workoutClass");
+		
+		Resource yoga = model.createResource(owl + "yoga");
+		Resource pilates = model.createResource(owl + "pilates");
+		
+		Resource pulsTopp55 = model.createResource(owl + "pulstopp 55");
+		Resource intervall45 = model.createResource(owl + "intervall 45");
+		Resource intervall55 = model.createResource(owl + "intervall 55");
+		
+		Resource stepMoves = model.createResource(owl + "step moves");
+		Resource tabata = model.createResource(owl + "tabata");
+		Resource styrkeStang = model.createResource(owl + "styrke med stang");
+		
+		Resource aqualintervall = model.createResource(owl + "aqual intervall");
+		
+		Resource intervallP = model.createResource(owl + "intervall pyramide");
 
-		Resource[]listOfInterestsInes = {resBikeriding, resMusic, resTravelling};
-		Resource[]listOfInterestsCade = {resBirds, resEcology, resTravelling, resEnviroment, resPhotography};
+		Resource[]listOfTrainingFormsSIB = {yoga, pilates, };
+		Resource ssClasses = model.createList(listOfTrainingFormsSIB);
+		//studentsenteret.addProperty(workoutClass, ssClasses);
 		
-		Resource inesIntersts = model.createList(listOfInterestsInes);
-		Resource cadeIntersts = model.createList(listOfInterestsCade);
 
-		inesD.addProperty(propIntrests, inesIntersts);
-		cadeTracy.addProperty(propIntrests, cadeIntersts);
+		//starttid
+		Property starttid = model.
+		
+		//varighet
+
 		
 		//Writing to file
 /*		try {
