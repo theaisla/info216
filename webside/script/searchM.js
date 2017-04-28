751b36fed60fcf7edade42ee7a799a5db3a00c0b
martyup <martine.o.j@gmail.com>
2017-04-28 15:08:29 +0200

    jkk

---
+++ webside/script/searchM.js
@@ -0,0 +1,47 @@
+//___________________________________ Get elements _______________________________________________________________
+
+// Returns an array with values of the selected (checked) checkboxes
+function getElements() {
+  // array that stores selected checkboxes values
+  var selection = [];
+  // gets all the input tags in frm, and their number
+  var input = document.getElementsByTagName('input');
+
+  for(var i = 0; i < input.length; i++) {
+    if(input[i].type == 'checkbox' && input[i].checked == true) selection.push(input[i].value);
+  }
+  return selection;
+}
+
+// when button is clicked, return values
+document.getElementById('btn').onclick = function(){
+   // gets the array returned by getElements()
+  var results = getElements(this.form);
+  console.log(results);
+  document.getElementById("demo").innerHTML = (results);
+}
+
+//___________________________________ Create query _______________________________________________________________
+
+var query = "\
+prefix a: <http://example/SibCity> \
+SELECT ?timer ?starttid\
+WHERE {\
+  ?timer a:typeOfClass ?saltime .\
+  ?timer  a:Varighet "55 min" .\
+  ?timer  a:Start ?starttid\
+}";
+
+//___________________________________ Run query in fuseki ________________________________________________________
+
+
+
+//___________________________________ Get results ________________________________________________________________
+
+
+
+//___________________________________ Print results ______________________________________________________________
+
+
+
+//___________________________________ Run code ___________________________________________________________________
