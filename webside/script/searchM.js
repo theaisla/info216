3dedb53b41f3ce3d4d7c38cd65dbad03e1818e47
martyup <martine.o.j@gmail.com>
2017-05-02 01:04:10 +0200

    mh

--- webside/script/searchM.js
+++ webside/script/searchM.js
@@ -1,36 +1,155 @@
 //___________________________________ Get elements _______________________________________________________________
 
 // Returns an array with values of the selected (checked) checkboxes
-function getElements() {
-  // array that stores selected checkboxes values
-  var selection = [];
+function getValues() {
+  // arrays that store selected checkboxes values and names
+  var values = [];
   // gets all the input tags in frm, and their number
   var input = document.getElementsByTagName('input');
 
   for(var i = 0; i < input.length; i++) {
-    if(input[i].type == 'checkbox' && input[i].checked == true) selection.push(input[i].value);
+    if(input[i].type == 'checkbox' && input[i].checked == true) values.push(input[i].value);
   }
-  return selection;
+  return values;
 }
+
+function getNames() {
+  // arrays that store selected checkboxes values and names
+  var names = [];
+  // gets all the input tags in frm, and their number
+  var input = document.getElementsByTagName('input');
+
+  for(var i = 0; i < input.length; i++) {
+    if(input[i].type == 'checkbox' && input[i].checked == true) names.push(input[i].name);
+  }
+  return names;
+}
+
+function goThroughNames() {
+  var names = getNames(this.form);
+  var temp = {};
+  // check if two names are the same
+  for(x in names){
+    temp[names[x]] = true;
+    var r = [];
+  }
+  // Push names to array with a ?
+    for (var k in temp){
+    r.push("?" + k);
+
+    //(testarray.join("%"))
+  }
+  return r.join(" ");
+}
+
+function goThroughValues() {
+  var names = getNames(this.form);
+  var values = getValues(this.form);
+/*
+  var res = [];
+
+  for(i in names){
+//gjelder a:mandag/tirsdag.. a:saltime/spinning/loepetime
+    res.push("?" + names[i] + " ?x a:"  + values[i] + "</br>");
+  }
+  return res;
+  */
+  var res2 = [];
+
+  for(i in names){
+//gjelder tider "18:00"/ "60 min"
+    res2.push("?" + names[i] + " a:"  + names[i] + ' "' + values[i]+ '"' + "</br>");
+  }
+  return res2;
+}
+
 
 // when button is clicked, return values
 document.getElementById('btn').onclick = function(){
    // gets the array returned by getElements()
-  var results = getElements(this.form);
-  console.log(results);
-  document.getElementById("demo").innerHTML = (results);
+  var resultsVal = getValues(this.form);
+  var resultsName = getNames(this.form);
+  console.log(resultsVal);
+  console.log(resultsName);
+  /*document.getElementById("demo").innerHTML = (results);
+var ukedag = goThroughNames() + " ?x a:" + resultsVal  + " .";
+var startpunkt = goThroughNames() + " a:" + resultsName + " '" + resultsVal + "'" + " .";
+
+document.getElementById("demo").innerHTML =
+("prefix a: <http://example/SibCity> </br> SELECT "
+  //+ for (x in resultsNames){
+  //x;
+  //}
+ // + goThroughNames()
+ + "*"
+  + "</br> WHERE { </br>"
+  + ukedag 
+  + startpunkt 
+  + "</br> }"
+
+  );*/
+  document.getElementById("demo").innerHTML =
+("prefix a: <http://example/SibCity> </br> SELECT "
+  + goThroughNames()
+  + "</br> WHERE { </br> "
+  + goThroughValues()
+  + "}"
+
+  );
+
+
+
 }
+/*
 
-//___________________________________ Create query _______________________________________________________________
+SELECT ?Dag ?Start
+WHERE { 
+?Dag a:Onsdag
+,?Start a:07:00
 
-var query = "\
-prefix a: <http://example/SibCity> \
-SELECT ?timer ?starttid\
-WHERE {\
-  ?timer a:typeOfClass ?saltime .\
-  ?timer  a:Varighet "55 min" .\
-  ?timer  a:Start ?starttid\
-}";
+SELECT ?Dag
+WHERE { 
+?Dag ?x a:Onsdag.
+?Dag a:Start "07:00"
+
+  ?timer ?p a:Mandag .
+  ?timer  a:Varighet "55 min" .
+  ?timer  a:Start ?starttid
+  
+  
+?Dag ?x a:Torsdag.
+?Dag a:Start "07:00"
+
+
+//distanse
+? 
+
+//dager
+if (alleDager = ?Dag)
+	
+if (mandag = ?Mandag)
+	 ?timer ?p a:Mandag 
+//tidspunkt
+if (tidspunkt = ?Start)
+	  ?timer  a:Start ?starttid
+  ?timer  a:Start "16:00"
+  
+//type trening
+if (pilates = ?pilates)
+ a:Pilates ?x ?p 	
+
+//vartighet
+if(30 min eller mer = ?Varighet)
+  ?timer  a:Varighet "55 min" .
+    ?timer  a:Varighet ?min 
+
+	//fasilitet
+if(?harFasilitet)
+	  ?x a:harFasilitet ?p 
+
+
+*/
+
 
 //___________________________________ Run query in fuseki ________________________________________________________
 
