cc34d50958039c21e959ebfc4f08113a3f19aa30
martyup <martine.o.j@gmail.com>
2017-05-20 13:44:45 +0200

    blæ

--- webside/script/searchM2.js
+++ webside/script/searchM2.js
@@ -1,3 +1,20 @@
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://example.com/data.json'); // by default async 
+xhr.responseType = 'json'; // in which format you expect the response to be
+
+
+xhr.onload = function() {
+  if(this.status == 200) {// onload called even on 404 etc so check the status
+   console.log(this.response); // No need for JSON.parse()
+  }
+};
+
+xhr.onerror = function() {
+  // error 
+};
+
+
+xhr.send();
 function createWHERE() {
 	// arrays that store selected checkboxes values and names
 	var values = [];
@@ -26,6 +43,18 @@ function createWHERE() {
 			res.push("FILTER (?varighet < " + '"60"'+ ") . <br>");}
 			if (values[x] == "30til60"){
 			res.push("FILTER (?varighet <= " + '"60"'+ " && ?varighet > " + '"30"' + ") . <br>");}
+		}		
+		if(names[x] == "startTime"){
+			if (values[x] == "f10"){
+			res.push("FILTER (?startTime <= " + '"30"'+ ") . <br>");}
+			if (values[x] == "10til15"){
+			res.push("FILTER (?varighet < " + '"60"'+ ") . <br>");}
+			if (values[x] == "15til17"){
+			res.push("FILTER (?varighet <= " + '"60"'+ " && ?varighet > " + '"30"' + ") . <br>");}
+			if (values[x] == "18til20"){
+			res.push("FILTER (?varighet <= " + '"30"'+ ") . <br>");}
+			if (values[x] == "etter20"){
+			res.push("FILTER (?varighet < " + '"60"'+ ") . <br>");}
 		}
 	}
 
@@ -39,5 +68,5 @@ document.getElementById('btn').onclick = function createQuery(){
     + createWHERE() +
     " ?timer a:dayOfWeek ?dag . <br> ?timer a:duration ?varighet . <br> ?timer a:legalName ?sted . <br> ?timer a:title ?timeNavn . <br> ?timer a:startTime ?starter . <br> ?timer a:location ?location . <br> ?timer a:instructor ?instructor . <br> ?timer a:typeof ?liktSom . <br> ?timer a:startDate ?dato }" );
 
-document.getElementById("demo").innerHTML = myQuery;
+document.getElementById("demo2").innerHTML = myQuery;
 }
