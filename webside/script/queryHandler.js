f384b5fca1e8c6900744c6d21f3d8753a513ac4f
TheaIsla <thea.isla@gmail.com>
2017-05-20 16:41:14 +0200

    thea ting

--- webside/script/queryHandler.js
+++ webside/script/queryHandler.js
@@ -1,25 +1,14 @@
-function createWHERE() {
-  // arrays that store selected checkboxes values and names
-  var values = []; var names = [];
-  var listTime = [];   var listDay = [];  var listDuration = []; var listTitle = [];
-  var res = [];
+// arrays that store selected checkboxes values and names
+var values = []; var names = [];
+var listTime = [];   var listDay = [];  var listDuration = []; var listTitle = [];
+var res = [];
 
 
-  // gets all the input tags in frm, and their number
-  var input = document.getElementsByTagName('input');
+// gets all the input tags in frm, and their number
+var input = document.getElementsByTagName('input');
 
-  for(var i = 0; i < input.length; i++) {
-    if(input[i].type == 'checkbox' && input[i].checked == true) values.push(input[i].value);
-  }
 
-  for(var i = 0; i < input.length; i++) {
-    if(input[i].type == 'checkbox' && input[i].checked == true) names.push(input[i].name);
-  }
-
-  function join(){
-    return listDuration.join(" || ");
-  }
-
+function findValues() {
   //______ Find values _____
   for(x in values){
     if(names[x] == "dayOfWeek"){
@@ -44,16 +33,26 @@ function createWHERE() {
     }
 
     if(names[x] == "duration"){
-			if (values[x] == "min30"){
-			  listDuration.push("?varighet <= &quot30&quot");}
-			if (values[x] == "plus60"){
-			  listDuration.push("?varighet < &quot60&quot");}
-			if (values[x] == "30til60"){
-			  listDuration.push("?varighet <= &quot60&quot && ?varighet > &quot30&quot");}
-		}
+      if (values[x] == "min30"){
+        listDuration.push("?varighet <= &quot30&quot");}
+      if (values[x] == "plus60"){
+        listDuration.push("?varighet < &quot60&quot");}
+      if (values[x] == "30til60"){
+        listDuration.push("?varighet <= &quot60&quot && ?varighet > &quot30&quot");}
+    }
+  }
+}
+
+
+function createWHERE() {
+  for(var i = 0; i < input.length; i++) {
+    if(input[i].type == 'checkbox' && input[i].checked == true) values.push(input[i].value);
+  }
+  for(var i = 0; i < input.length; i++) {
+    if(input[i].type == 'checkbox' && input[i].checked == true) names.push(input[i].name);
   }
 
-//______ Print Query Text _____
+ findValues();
 
 for (x in names){
   if(names[x] == "dayOfWeek"){
@@ -66,20 +65,23 @@ for (x in names){
   }
 
   if(names[x] == "sameAs"){
-    res.push("?timer a:isSimilarTo ?value2 .  <br>FILTER(?value2 IN("
-    + listTitle + "))");
+    var listTitle2 = Array.from(new Set(listTitle));
+    res.push("?timer a:sameAs ?value2 .  <br>FILTER(?value2 IN("
+    + listTitle2 + "))");
   }
 
   if(names[x] == "duration"){
     res.push("FILTER(" + listDuration.join("||") + ")");
   }
 }
-
-
+// remove duplicates
   var result = Array.from(new Set(res));
 
-  console.log(result);
   return result.join(" . <br>");
+}
+
+
+function createAltWHERE() {
 
 }
 
@@ -91,14 +93,15 @@ document.getElementById('btn').onclick = function createQuery(){
 
   var myQuery = ("prefix a: &lthttp://schema.org/&gt <br> SELECT ?timeNavn ?starter ?dag ?varighet ?sted ?location <br>"
     + " WHERE { <br> ?timer a:dayOfWeek ?dag . <br> ?timer a:duration ?varighet . <br> ?timer a:legalName ?sted . <br> ?timer a:title ?timeNavn . <br> ?timer a:startTime ?starter . <br>?timer a:location ?location .  <br><br>"
-    + createWHERE() + "<br>}");
+    + createWHERE() + "<br>} <br><br>  <h3> Hvis ikke du fant det du ser etter, kanskje noen av disse resultatene faller med i smak:</h3>");
 
-    var myAltQuery = ("<h3> Hvis ikke du fant det du ser etter, kanskje noen av disse resultatene faller med i smak:</h3>"
-      + "prefix a: &lthttp://schema.org/&gt <br> SELECT ?timeNavn ?starter ?dag ?varighet ?sted ?location <br>"
+    var myAltQuery = ("prefix a: &lthttp://schema.org/&gt <br> SELECT ?timeNavn ?starter ?dag ?varighet ?sted ?location <br>"
       + " WHERE { <br> ?timer a:dayOfWeek ?dag . <br> ?timer a:duration ?varighet . <br> ?timer a:legalName ?sted . <br> ?timer a:title ?timeNavn . <br> ?timer a:startTime ?starter . <br>?timer a:location ?location .  <br><br>"
-      + createWHERE() + "<br>}");
+      + createAltWHERE() + "<br>}");
 
 document.getElementById("demo").innerHTML = myQuery;
+
+
 document.getElementById("alt").innerHTML = myAltQuery;
 
 }
